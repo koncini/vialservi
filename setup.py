ab78c24b96c7ff165e7c1a3fdda39aac60baaf35
koncini <juan.rincon8205@gmail.com>
2019-11-13 16:34:32 -0500

    Instalador para windows

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from cx_Freeze import setup, Executable
 setup(name="Ventana",
       version="0.1",
       description="Ventana",
-      executables=[Executable("analizar.py", base="Win32GUI")],)
+      executables=[Executable("analizar.py", base="Win32GUI", requires=['openpyxl'])])
